3d804e3b5d4ca591050e7e0a3b79cba76c6c6463
orange11th <orange1th@gmail.com>
2022-08-06 10:31:07 +0900

    0.0

--- app/src/main/java/com/cookandroid/with/LoginActivity.java
+++ app/src/main/java/com/cookandroid/with/LoginActivity.java
@@ -80,6 +80,7 @@ public class LoginActivity extends AppCompatActivity {
                 cursor.close();
             }
         });
+        /*
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,23 +104,23 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        /*
+
+         */
         btnRegister.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
                 //회원가입 버튼 클릭
-                Toast toast = Toast.makeText(LoginActivity.this, "회원가입 화면으로 이동", Toast.LENGTH_SHORT);
-                toast.show();
                 Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
                 startActivity(intent);
                 //finish();
             }
-        });*/
+        });
     }
 }
 class DatabaseOpenHelper extends SQLiteOpenHelper{
 
-    public static final String tableName = "Users";
+    public static final String tableName = "TestDB";
 
     public DatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -136,7 +137,7 @@ class DatabaseOpenHelper extends SQLiteOpenHelper{
     }
 
     public void createTable(SQLiteDatabase db){
-        String sql = "CREATE TABLE " + tableName + "(id text, pw text)";
+        String sql = "CREATE TABLE " + tableName + "(id text, pw text, name text, phone text, region text, address text, gender text, birth datetime )";
         try {
             db.execSQL(sql);
         }catch (Exception e){
--- app/src/main/java/com/cookandroid/with/RegisterActivity.java
+++ app/src/main/java/com/cookandroid/with/RegisterActivity.java
@@ -1,8 +1,18 @@
 package com.cookandroid.with;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +20,29 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Spinner spinGender=findViewById(R.id.spinGender);
+        final String[] gender={"남","여"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, gender);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinGender.setAdapter(adapter);
+
+        ArrayList<Button> btnLIst=new ArrayList<Button>();
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("회원가입");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home: {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
