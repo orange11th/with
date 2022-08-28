192fc6707b8931b148e66790fa95487786444dd6
oxxxzy <heezy109@gmail.com>
2022-08-29 00:18:13 +0900

    220829_1218

---
+++ app/src/main/java/com/cookandroid/with/SeniorProfileActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.with;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeniorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senior_profile);
+    }
+}
