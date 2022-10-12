ad539219bb3f011935708b2ae607ab9a866fc382
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-12 19:45:15 +0900

    1012 병훈 - 결제 내역 화면

---
+++ app/src/main/java/com/cookandroid/with/CashHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.with;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash_history);
+    }
+}
--- app/src/main/java/com/cookandroid/with/SeniorProfileActivity.java
+++ app/src/main/java/com/cookandroid/with/SeniorProfileActivity.java
@@ -2,15 +2,41 @@ package com.cookandroid.with;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /*written by 병훈*/
 
+/* 필요 기능
+* 1. 결제 버튼
+* 2. 수정 버튼
+* 3. 설정 버튼*/
+
 public class SeniorProfileActivity extends AppCompatActivity {
+
+    ImageView btn_cash;// 결제 버튼 선언
+    ImageView btn_revise; //수정 버튼 선언
+    ImageView btn_setting; //세팅 버튼 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senior_profile);
+
+        btn_cash = (ImageView) findViewById(R.id.btn_cash);
+        btn_revise = (ImageView) findViewById(R.id.btn_revise);
+        btn_setting = (ImageView) findViewById(R.id.btn_setting);
+
+        //화면전환 - 결제 버튼 누르면 결제내역화면(CashHistoryActivity)으로 이동
+        btn_cash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CashHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
